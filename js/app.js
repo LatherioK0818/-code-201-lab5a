dea28dac01c4fa71ffbe658ccbb1405e58f79c77
Latherio Kidd <kidd.latherio@gmail.comS>
2023-10-28 19:57:46 -0400

    updates to question 4

--- js/app.js
+++ js/app.js
@@ -66,7 +66,7 @@ console.log(result[2]);
 console.log(result[3]);
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -81,15 +81,23 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+function sumArray(sumArr) {
+  var sumResult = 0;
 
-function sumArray(sumArr) { //eslint-disable-line
+  for (var i = 0; i < sumArr.length; i++) {
+    // Use the sum function to calculate the sum of elements in the array
+    sumResult = sum(sumResult, sumArr[i])[0];
+  }
 
+  var numbers = sumArr.join(',');
+  var message = numbers + ' was passed in as an array of numbers, and ' + sumResult + ' is their sum.';
+  return [sumResult, message];
 }
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
